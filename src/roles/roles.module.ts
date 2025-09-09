961b357ee4ca67dacd2f301974d747c370cd6df9
Ernesto Canquiz <caribestic123@gmail.com>
2025-09-08 20:38:39 -0400

    fix: It does not show the roles

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
 import { Role } from './entities/role.entity';
 import { Permission } from '../permissions/entities/permission.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role, Permission])],
+  imports: [
+    TypeOrmModule.forFeature([Role, Permission]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [RolesController],
   providers: [RolesService],
   exports: [RolesService],
