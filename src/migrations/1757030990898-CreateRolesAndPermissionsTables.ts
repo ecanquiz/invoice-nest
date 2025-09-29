30ff46cf5dfd2c65c7079af70ee93e060e10574c
Ernesto Canquiz <caribestic123@gmail.com>
2025-09-29 18:30:01 -0400

    fix: Error runing migrations

--- src/migrations/1757030990898-CreateRolesAndPermissionsTables.ts
+++ src/migrations/1757030990898-CreateRolesAndPermissionsTables.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateRolesAndPermissionsTables1757030990898 implements MigrationInterface {
-    name = 'CreateRolesAndPermissionsTables1712345678901'
+    name = 'CreateRolesAndPermissionsTables1757030990898'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         // Permissions table
